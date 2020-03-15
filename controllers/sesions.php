b710c4d3add831beee1034173f737610e10c6b62
alexp11 <edisonm@unicauca.edu.co>
2020-03-14 19:01:36 -0500

    Agregados las vistas visual y auditiva y modificada la vista de registro

--- controllers/sesions.php
+++ controllers/sesions.php
@@ -3,6 +3,16 @@ session_start();
 
 include_once('../db/queries.php');
 
-if (condition) {
-    # code...
-}
+if (!isset($_POST['email']) && !isset($_POST['pass'])) {
+    if (queries::signIn($_POST["email"], $_POST["pass"]) == true) {
+        if($_SESSION['discapacidad'] == '1'){
+           header('location:../views/homevis.php');
+        }
+        else{
+            header('location:../views/homeaud.php');
+        }
+    } else {
+        header('location:../views/signIn.php');
+    }
+} 
+?>
